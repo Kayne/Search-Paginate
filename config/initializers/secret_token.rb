bcadb8bdd41adeaef147c0a209825b1cc686bab5
Marcin Bratek <github@bratekmarcin.eu>
2011-11-05 13:58:16 +0100

    New repository for search rails application

---
+++ config/initializers/secret_token.rb
@@ -0,0 +1,7 @@
+# Be sure to restart your server when you modify this file.
+
+# Your secret key for verifying the integrity of signed cookies.
+# If you change this key, all old signed cookies will become invalid!
+# Make sure the secret is at least 30 characters and all random,
+# no regular words or you'll be exposed to dictionary attacks.
+Search::Application.config.secret_token = 'ff66e1c72fd07b6c451d6b3428584e2c7a5fdb9eae14fb21a4d5896d6a61b3079a994a32aff442474914120f360ee0b8578eec2c47244adccc077b9421c28670'
