f66e5f49d54b33f13732596e897f2d63c8162f11
Marcin Bratek <github@bratekmarcin.eu>
2011-11-05 14:04:15 +0100

    New migration CreatePatientsTable

---
+++ db/migrate/20111105130118_create_patients_table.rb
@@ -0,0 +1,10 @@
+class CreatePatientsTable < ActiveRecord::Migration
+  def change
+    create_table :patients do |t|
+      t.string :name
+      t.string :surname
+      t.integer :age
+      t.timestamps
+    end
+  end
+end
