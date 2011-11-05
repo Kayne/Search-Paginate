f4e287780cf4c513f98af3ca73131af0205fe718
Marcin Bratek <github@bratekmarcin.eu>
2011-11-05 14:11:26 +0100

    Fix error in patient.rb (typo) and add routes

--- app/models/patient.rb
+++ app/models/patient.rb
@@ -1,3 +1,3 @@
-class Patient < AcrtiveRecord::Base
+class Patient < ActiveRecord::Base
 
 end
--- config/routes.rb
+++ config/routes.rb
@@ -12,6 +12,7 @@ Search::Application.routes.draw do
 
   # Sample resource route (maps HTTP verbs to controller actions automatically):
   #   resources :products
+  resources :patients
 
   # Sample resource route with options:
   #   resources :products do
@@ -48,11 +49,11 @@ Search::Application.routes.draw do
 
   # You can have the root of your site routed with "root"
   # just remember to delete public/index.html.
-  # root :to => 'welcome#index'
+  root :to => 'patients#index'
 
   # See how all your routes lay out with "rake routes"
 
   # This is a legacy wild controller route that's not recommended for RESTful applications.
   # Note: This route will make all actions in every controller accessible via GET requests.
-  # match ':controller(/:action(/:id(.:format)))'
+  match ':controller(/:action(/:id(.:format)))'
 end
